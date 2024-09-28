8cda5109793e1989e7256eb24909e3b08fe0c84f
Jarell Tamonte <jarelltamonte@gmail.com>
2024-09-28 11:29:11 +0800

    backend update

---
+++ coffee-store-backend/data/products.js
@@ -0,0 +1,10 @@
+const products = [
+    { id: 1, name: 'Espresso', price: 5, description: 'Rich and bold espresso', image: 'espresso.jpg' },
+    { id: 2, name: 'Cappuccino', price: 6, description: 'Creamy cappuccino', image: 'cappuccino.jpg' },
+    { id: 3, name: 'Latte', price: 5.5, description: 'Smooth latte with milk', image: 'latte.jpg' },
+    { id: 3, name: 'Cream Cheese Boba', price: 6.5, description: 'Creamy Milk Tea', image: 'creamcheesebobs.jpg' },
+    { id: 3, name: 'Tropical Juice', price: 4.5, description: 'Cool Tropical Juice', image: 'tropicaljuice.jpg' },
+    { id: 3, name: 'Macchiato', price: 6, description: 'Delicious Macchiato', image: 'macchiato.jpg' },
+  ];
+  
+  module.exports = products;
